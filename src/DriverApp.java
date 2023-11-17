e183b0a54317990ef3f7c07a15249af13889423f
DiegoAJF <diejimenez@estudiantec.cr>
2023-11-17 01:47:09 -0600

    Se añadio el mapa

--- src/DriverApp.java
+++ src/DriverApp.java
@@ -1,16 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DriverApp extends JFrame {
 
+    private MapaInteractivo mapaInteractivo;
 
     public DriverApp(String username) {
         setTitle("Welcome, " + username + "!");
-        setSize(400, 300);
+        setSize(700, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // Centra la ventana en la pantalla
+        setLocationRelativeTo(null);
 
         // Crear JMenuBar
         JMenuBar menuBar = new JMenuBar();
@@ -20,36 +21,18 @@ public class DriverApp extends JFrame {
 
         // Crear JMenuItem "View Average Rating"
         JMenuItem viewRatingItem = new JMenuItem("View Average Rating");
-        viewRatingItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Lógica para visualizar la calificación promedio
-                JOptionPane.showMessageDialog(DriverApp.this, "Viewing Average Rating");
-            }
-        });
+        viewRatingItem.addActionListener(e -> JOptionPane.showMessageDialog(DriverApp.this, "Viewing Average Rating"));
 
         // Crear JMenuItem "View Friendships with Drivers"
         JMenuItem viewFriendshipsItem = new JMenuItem("View Friendships with Employees");
-        viewFriendshipsItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Lógica para visualizar relaciones de amistad con conductores
-                JOptionPane.showMessageDialog(DriverApp.this, "Viewing Friendships with Employees");
-            }
-        });
+        viewFriendshipsItem.addActionListener(e -> JOptionPane.showMessageDialog(DriverApp.this, "Viewing Friendships with Employees"));
 
         // Crear JMenuItem "Add Friends"
         JMenuItem addFriendsItem = new JMenuItem("Add Friends");
-        addFriendsItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Lógica para añadir amigos
-                String friendName = JOptionPane.showInputDialog(DriverApp.this, "Enter friend's name:");
-                if (friendName != null) {
-                    // Aquí puedes realizar acciones con el nombre del amigo
-                    // Por ejemplo, mostrar un mensaje con el nombre
-                    JOptionPane.showMessageDialog(DriverApp.this, "Friend added: " + friendName);
-                }
+        addFriendsItem.addActionListener(e -> {
+            String friendName = JOptionPane.showInputDialog(DriverApp.this, "Enter friend's name:");
+            if (friendName != null) {
+                JOptionPane.showMessageDialog(DriverApp.this, "Friend added: " + friendName);
             }
         });
 
@@ -64,16 +47,72 @@ public class DriverApp extends JFrame {
         // Establecer el JMenuBar en el JFrame
         setJMenuBar(menuBar);
 
+        // Crear el mapa interactivo con 30 destinos
+        mapaInteractivo = new MapaInteractivo(30);
+
+        // Agregar el mapa a la ventana
+        add(mapaInteractivo, BorderLayout.CENTER);
+
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Ejemplo de uso
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new DriverApp("Driver123").setVisible(true);
-            }
-        });
+        SwingUtilities.invokeLater(() -> new DriverApp("Driver123"));
+    }
+}
+
+class MapaInteractivo extends JPanel {
+
+    private Map<String, Punto> puntos;
+
+    public MapaInteractivo(int numDestinos) {
+        puntos = new HashMap<>();
+        generarMapa(numDestinos);
+    }
+
+    private void generarMapa(int numDestinos) {
+        // Crear posiciones caóticas para los destinos
+        int[] posX = {80, 180, 280, 380, 480, 120, 220, 320, 420, 520, 80, 180, 280, 380, 480, 120, 220, 320, 420, 520, 80, 180, 280, 380, 480, 120, 220, 320, 420, 520};
+        int[] posY = {50, 100, 50, 100, 50, 200, 200, 200, 200, 200, 350, 300, 350, 300, 350, 450, 400, 450, 400, 450, 550, 500, 550, 500, 550, 600, 600, 600, 600, 600};
+
+        // Crear puntos (destinos) con posiciones caóticas
+        for (int i = 0; i < numDestinos; i++) {
+            String nombre = "Destino " + (i + 1);
+            Punto punto = new Punto(nombre, posX[i] + getRandomVariation(), posY[i] + getRandomVariation());
+            puntos.put(nombre, punto);
+        }
+    }
+
+    private int getRandomVariation() {
+        return (int) (Math.random() * 30 - 15); // Variación entre -15 y 15
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Dibujar puntos sin conexiones
+        for (Punto punto : puntos.values()) {
+            punto.dibujar(g);
+        }
+    }
+}
+
+class Punto {
+    private String nombre;
+    private int x, y;
+
+    public Punto(String nombre, int x, int y) {
+        this.nombre = nombre;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void dibujar(Graphics g) {
+        // Dibujar el punto
+        g.setColor(Color.BLUE);
+        g.fillOval(x - 10, y - 10, 20, 20);
+        g.setColor(Color.BLACK);
+        g.drawString(nombre, x - 10, y - 20);
     }
 }
--- src/EmployeeApp.java
+++ src/EmployeeApp.java
@@ -1,33 +1,17 @@
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EmployeeApp extends JFrame {
     private JComboBox<String> transportationComboBox;
+    private MapaInteractivoE mapaInteractivo;
 
     public EmployeeApp(String username) {
         setTitle("Welcome, " + username + "!");
-        setSize(400, 300);
+        setSize(800, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // Centra la ventana en la pantalla
+        setLocationRelativeTo(null);
 
         // Crear y agregar JComboBox en la parte inferior
         String[] transportationOptions = {"Friend", "Driver"};
@@ -36,109 +20,72 @@ public class EmployeeApp extends JFrame {
 
         // Crear JMenuBar
         JMenuBar menuBar = new JMenuBar();
-
-        // Crear JMenu "Options"
         JMenu optionsMenu = new JMenu("Options");
-
-        // Crear JMenuItem "View Average Rating"
         JMenuItem viewRatingItem = new JMenuItem("View Average Rating");
-        viewRatingItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Lógica para visualizar la calificación promedio
-                JOptionPane.showMessageDialog(EmployeeApp.this, "Viewing Average Rating");
-            }
-        });
-
-        // Crear JMenuItem "View Friendships with Drivers"
-        JMenuItem viewFriendshipsItem = new JMenuItem("View Friendships with Drivers");
-        viewFriendshipsItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Lógica para visualizar relaciones de amistad con conductores
-                String transportationOption = getTransportationOption();
-                if ("Driver".equals(transportationOption)) {
-                    // Lógica específica para visualizar amistades con conductores
-                    viewDriverFriendships();
-                } else {
-                    // Lógica para otros tipos de transporte (si es necesario)
-                    JOptionPane.showMessageDialog(EmployeeApp.this, "Viewing Friendships with " + transportationOption + "s");
-                }
-            }
-        });
-
-        // Agregar los JMenuItem al JMenu
+        viewRatingItem.addActionListener(e -> JOptionPane.showMessageDialog(EmployeeApp.this, "Viewing Average Rating"));
+        JMenuItem viewFriendshipsItem = new JMenuItem("View Friendships");
         optionsMenu.add(viewRatingItem);
         optionsMenu.add(viewFriendshipsItem);
-
-        // Agregar el JMenu al JMenuBar
         menuBar.add(optionsMenu);
-
-        // Establecer el JMenuBar en el JFrame
         setJMenuBar(menuBar);
+
+        // Crear el mapa interactivo con 30 destinos
+        mapaInteractivo = new MapaInteractivoE(30);
+        add(mapaInteractivo, BorderLayout.CENTER);
 
         setVisible(true);
     }
 
-    // Agrega un método para obtener la opción seleccionada en el JComboBox
     public String getTransportationOption() {
         return (String) transportationComboBox.getSelectedItem();
     }
-// Dentro de la clase EmployeeApp
 
-    // Dentro de la clase EmployeeApp
-    private void viewDriverFriendships() {
-        // Lógica para obtener y mostrar las amistades con conductores
-        List<String> driverFriendships = getDriverFriendshipsWithDetails();  // Obtener detalles de las amistades con conductores
-        if (!driverFriendships.isEmpty()) {
-            // Si hay conductores amigos, mostrar en un cuadro de diálogo
-            String message = "Friendships with Drivers:\n" + String.join("\n", driverFriendships);
-            JOptionPane.showMessageDialog(EmployeeApp.this, message);
-        } else {
-            JOptionPane.showMessageDialog(EmployeeApp.this, "No friendships with drivers.");
-        }
-    }
-
-    // Agregar un método para obtener los detalles de las amistades con conductores
-    private List<String> getDriverFriendshipsWithDetails() {
-        // Lógica para obtener los detalles de las amistades con conductores desde el archivo XML
-        List<String> driverFriendships = new ArrayList<>();
-
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(new File("friendships.xml"));
-
-            NodeList friendshipNodes = doc.getElementsByTagName("friendship");
-            for (int i = 0; i < friendshipNodes.getLength(); i++) {
-                Node friendshipNode = friendshipNodes.item(i);
-                if (friendshipNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element friendshipElement = (Element) friendshipNode;
-                    String friend = friendshipElement.getElementsByTagName("friend").item(0).getTextContent();
-                    String requestDetails = friendshipElement.getElementsByTagName("requestDetails").item(0).getTextContent();
-                    // Agregar el nombre del amigo y detalles de la solicitud a la lista
-                    driverFriendships.add(friend + " - Request Details: " + requestDetails);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            // Manejar la excepción según tus necesidades
-        }
-
-        return driverFriendships;
-    }
-
-
-    // Agregar un método para leer el contenido de un archivo
-    private static String readFileContents(File file) throws IOException {
-        StringBuilder content = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-        }
-        return content.toString();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new EmployeeApp("Employee123"));
     }
 }
 
+class MapaInteractivoE extends JPanel {
+    private Map<String, Punto> puntos;
+
+    public MapaInteractivoE(int numDestinos) {
+        puntos = new HashMap<>();
+        generarMapa(numDestinos);
+    }
+
+    private void generarMapa(int numDestinos) {
+        int[] posX = {80, 180, 280, 380, 480, 120, 220, 320, 420, 520, 80, 180, 280, 380, 480, 120, 220, 320, 420, 520, 80, 180, 280, 380, 480, 120, 220, 320, 420, 520};
+        int[] posY = {50, 100, 50, 100, 50, 200, 200, 200, 200, 200, 350, 300, 350, 300, 350, 450, 400, 450, 400, 450, 550, 500, 550, 500, 550, 600, 600, 600, 600, 600};
+        for (int i = 0; i < numDestinos; i++) {
+            String nombre = "Destino " + (i + 1);
+            Punto punto = new Punto(nombre, posX[i], posY[i]);
+            puntos.put(nombre, punto);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (Punto punto : puntos.values()) {
+            punto.dibujar(g);
+        }
+    }
+}
+
+class PuntoE {
+    private String nombre;
+    private int x, y;
+
+    public PuntoE(String nombre, int x, int y) {
+        this.nombre = nombre;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillOval(x - 10, y - 10, 20, 20);
+        g.setColor(Color.BLACK);
+        g.drawString(nombre, x - 10, y - 20);
+    }
+}
